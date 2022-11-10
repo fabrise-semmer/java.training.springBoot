f44da33f4a89e94f93456ae1a71afc52bedebef9
Fabrise Karina Semmer <117269225+fabrise-semmer@users.noreply.github.com>
2022-11-10 08:52:25 +0000

    adding the Topic Package

---
+++ springBoot.Database/src/main/java/springBoot/Database/Topic/TopicController.java
@@ -0,0 +1,49 @@
+package springBoot.Database.Topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	// mark to Spring that need dependency injection
+	// when Spring creates an instance of TopicController he is going to look all the members,
+	// and he is going to see if any of them has a dependency of TopicService, and it will inject that
+	@Autowired 	
+	private TopicService topicService;
+	
+	@RequestMapping("/topics")
+	public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{id}")
+	public Topic getOneTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicService.addTopic(topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicService.updateTopic(id, topic);
+	}
+	
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicService.deleteTopic(id);
+	}
+
+
+
+}
+
